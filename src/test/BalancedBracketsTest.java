a083385e5db1af1d9d571c26195c66ebb0db7558
Heather Hecky <hhecky@gmail.com>
2020-09-02 20:15:13 -0500

    finished studio

--- src/test/BalancedBracketsTest.java
+++ src/test/BalancedBracketsTest.java
@@ -1,7 +1,7 @@
 package test;
 
+import main.BalancedBrackets;
 import org.junit.Test;
-
 import static org.junit.Assert.*;
 
 public class BalancedBracketsTest {
@@ -9,8 +9,51 @@ public class BalancedBracketsTest {
     //TODO: add tests here
     @Test
     public void emptyTest() {
+
         assertEquals(true, true);
     }
-
-
+    @Test
+    public void onlyBracketReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]"));
+    }
+    @Test
+    public void bracketsWithStringReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[string]"));
+    }
+    @Test
+    public void bracketsInsideStringReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("str[ing]s"));
+    }
+    @Test
+    public void bracketsBehindStringReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("string[]"));
+    }
+    @Test
+    public void bracketsInFrontOfStringReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]string"));
+    }
+    @Test
+    public void twoSetsOfBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[][]"));
+    }
+    @Test
+    public void doubleBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[[]]"));
+    }
+    @Test
+    public void backwardsBracketsReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]["));
+    }
+    @Test
+    public void oneBracketReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[string"));
+    }
+    @Test
+    public void mismatchedBracketsReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[string[]"));
+    }
+    @Test
+    public void doubleLeftRightBracketsReturnFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]][["));
+    }
 }
